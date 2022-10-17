d7d57c17e1bf10d8983a512e06b5a3ca6edeebb7
kamahadev <112901164+kamahadev@users.noreply.github.com>
2022-10-17 12:47:41 +0200

    Added comments proposing a solution to be implemented

--- Detect_Pangram.py
+++ Detect_Pangram.py
@@ -4,3 +4,11 @@ A pangram is a sentence that contains every single letter of the alphabet at lea
 Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.s
 
 """
+"""
+Proposed solution:
+
+We can create a check list for all letters of the alphabet, when a letter is found we check in that check list 
+and if a letter that is already check is passed, we skip and when all the check list is fully checked. return true else false. 
+We also ignore numbers and punctions (special characters)
+
+"""
